620dbd2c3a073003f520b1b56ab18b72f72b4a01
Guidejirat <guidejirat@gmail.com>
2026-02-28 19:36:39 +0700

    add gitignore and remove node_modules

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-let name = "guideji";
-let age = 30;
-console.log("hello "+name);
-console.log("age = "+age);
+const prompt = require("prompt-sync")();
 
+let name = prompt("กรุณาใส่ชื่อของคุณ: ");
+let birthYear = prompt("กรุณาใส่ปีเกิด (ค.ศ.): ");
+
+let currentYear = new Date().getFullYear();
+let age = currentYear - Number(birthYear);
+
+console.log("สวัสดี " + name);
+console.log("ปีนี้คุณอายุประมาณ " + age + " ปี");
